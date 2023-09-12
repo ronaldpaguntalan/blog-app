2a0df9c9e2cc010705c23b5bec74e9be0653f452
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-09-12 18:16:06 +0800

    Angular Blog App - Sample Routing, added bootstrap, and global css and font
    Title:Angular Application From Scratch - Angular Project Blog App For Your Portfolio
    
    Tutorial: https://www.youtube.com/watch?v=6zv1PTuqIIc&list=RDCMUCVO85fbcuI-726-aNVErDmA&start_radio=1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'category', component: SingleCategoryComponent},
+  {path: 'post', component: SinglePostComponent},
+
+  {path: 'about', component: AboutComponent},
+  {path: 'terms-and-condition', component: TermsAndConditionComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
